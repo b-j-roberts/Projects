02918df9949fda5c8fb461159f38e7c50b9cca95
Brandon, the Master <brandonjroberts22@gmail.com>
2019-09-17 20:34:48 -0500

    finished k_nearest, some main setup

--- k-nearest-neighbors/k-nearest.cpp
+++ k-nearest-neighbors/k-nearest.cpp
@@ -53,26 +53,93 @@ const float metric<float>(const Point<float>& p1, const Point<float>& p2) {
   return sqrt( pow(p1.x() - p2.x(), 2) + pow(p1.y() - p2.y(), 2) );
 }
 
+// Holds position and value of element of a vector
+template<typename T>
+struct pos_holder {
+  T value;
+  size_t pos;
+  pos_holder(T val, int i): value(val), pos(i) { }
+};
+
+int min(int a, int b) { return a < b ? a : b; }
+
 // k_nearest returns a vector of the index of the k nearest neighbors in the given vector to a point passed
 template<typename T>
-const std::vector<int> k_nearest(const std::vector<Point<T>>& points, const Point<T>& point, int k) {  
+const std::vector<int> k_nearest(const std::vector<Point<T>>& points, const Point<T>& point, size_t k) {  
 
+  // Get distances between all points and passed point
   std::vector<T> dist(points.size());
   std::transform(points.begin(), points.end(), dist.begin(), [&](auto& p) { return metric(point, p); });
 
-  // HOW TO OBTAIN INDEX AFTER SORT ? 
+  // Group distance with vector position then sort by distance
+  std::vector<pos_holder<T>> pos;
+  for(size_t i = 0, end = points.size(); i < end; ++i) {
+    pos.emplace_back(dist[i], i);
+  }
+  std::sort(pos.begin(), pos.end(), [&](const pos_holder<T>& a,const pos_holder<T>& b) -> bool {
+    return a.value < b.value;
+  });
 
+  // Take k closest values and return the index of them
   std::vector<int> ret;
+  for(size_t i = 0, end = min(k, points.size()); i < end; ++i) ret.push_back(pos[i].pos);
   return ret;
 }
 
-// TO DO : Make a function like above but using a point inside of the vector, so we just pass its index ?
-//           Should we ignore this point in the search for the k nearest neighbors ? maybe add 1 to k ?
+// Does same thing as other k_nearest (see above) but for a point inside of the vector at position idx
+template<typename T>
+const std::vector<int> k_nearest(const std::vector<Point<T>>& points, size_t idx, size_t k) {
+   auto ret = k_nearest(points, points[idx], k + 1);
+   ret.erase(std::remove(ret.begin(), ret.end(), idx), ret.end());
+   return ret;
+}
 
 int main() {
- 
-  std::vector<Point<float>> pi = { Point<float>(2.2, 2.1), Point<float>(2.1, 4.0), Point<float>(3.4, 5.2) };
-  auto r = k_nearest(pi, pi[0], 1);
+
+  bool float_mode = false;
+  bool label_mode = false;
+  int neighbors = 0;
+  float radius;
+
+  // Ability to toggle float mode, Resets
+  // Ability to toggle label mode, Resets
+
+  if(float_mode) {
+    std::vector<Point<float>> float_points;
+    int index; // Set to most recent selection (or just keep it there till changed)
+   
+    // Ability to set neighbors, not reset
+
+    if(!label_mode) {
+      
+      std::vector<int> nearest_pos = k_nearest(float_points, index, neighbors);
+      // color nearest points
+      
+      radius = metric(float_points[index], float_points[nearest_pos[nearest_pos.size() - 1]]);
+
+    } else {
+      
+      Point<float> unlabeled; // Placed unlabeled point to find the k nearest to for label mode
+
+      std::vector<int> nearest_pos = k_nearest(float_points, unlabeled, neighbors);
+
+      // set unlabeled label based on predictor function (include weighted and unweighted option, togglable)
+
+      radius = metric(float_points[index], float_points[nearest_pos[nearest_pos.size() - 1]]);
+
+    }
+    // draw points (if in label mode draw labels too)
+    // draw circle
+    // reset colors
+
+  } else {
+    std::vector<Point<int>> int_points;
+    
+    // Ability to set neighbors
+  
+    // grid view
+
+  }
 
   return 0;
 }
@@ -83,3 +150,6 @@ int main() {
 //   allow placing of labels and then an identifier based on k-nearest (can change k), on a tie pick the nearest neighbor,
 //     on a tie for nearest neighbors include all ties too (highlight and make it pop up with a note) 
 //     on a tie for nearest neighbor on classification (ie ties in number and then ties in finding nearest (label) then pop up 50 / 50
+//   allow weighted option, for which closer elements count as more likely to affect label guess
+//
+//   No error handing for indexing in k_nearest
